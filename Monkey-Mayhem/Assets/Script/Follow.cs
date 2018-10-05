6b8b165b441139da1a232afe1ce9577129d03215
Jie <caicoding2@gmail.com>
2018-10-05 15:20:07 -0500

    Push

---
+++ Monkey-Mayhem/Assets/Script/Follow.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Follow : MonoBehaviour {
+
+    public GameObject player;
+    // Update is called once per frame
+    void LateUpdate () {//x.y.z
+        transform.position = new Vector3(0f,player.transform.position.y, -10f);
+	}
+}
---
+++ Monkey-Mayhem/Assets/Script/Spawn.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawn : MonoBehaviour {
+
+    public GameObject Obstacle;
+    public GameObject player;
+    [SerializeField]
+    private float SpawnInterval = 1f;
+    float nextSpawn;
+
+    Vector2 CamSize;
+
+	// Use this for initialization
+	void Start () {
+        CamSize = new Vector2(Camera.main.aspect * Camera.main.orthographicSize, Camera.main.aspect * Camera.main.orthographicSize);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Time.time > nextSpawn)
+        {
+            nextSpawn = Time.time + SpawnInterval;
+            Vector2 SpawnLocation = new Vector2(Random.Range(-CamSize.x, CamSize.x), player.transform.position.y + CamSize.y);
+            Instantiate(Obstacle, SpawnLocation, Quaternion.identity);
+        }
+    }
+}
