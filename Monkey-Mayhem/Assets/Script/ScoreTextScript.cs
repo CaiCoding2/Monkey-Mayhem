154f0c229c4f7b4154affc4b42ae2ba975af3efc
gmo3379 <gmo3379@gmail.com>
2018-10-07 20:58:14 -0500

    Total in game over screen

--- Monkey-Mayhem/Assets/Script/ScoreTextScript.cs
+++ Monkey-Mayhem/Assets/Script/ScoreTextScript.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using System.Net.Mime;
+using System.Xml.Schema;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -9,19 +10,22 @@ public class ScoreTextScript : MonoBehaviour
     Text bananaText;
     Text scoreText;
     Text timeText;
+    private Text totalText;
     
     public static int bananaAmount;
     public static float score;
     public static int seconds;
+    public int total = 0;
 
     private float realSeconds = 0;
     
     void Start()
     {
-        score = 0;
+        int total = 0;
         bananaText = GetComponent<Text>();
         scoreText = GetComponent<Text>();
         timeText = GetComponent<Text>();
+        totalText = GetComponent<Text>();
     }
 
     private void Update()
@@ -41,11 +45,17 @@ public class ScoreTextScript : MonoBehaviour
         {
             bananaText.text = bananaAmount.ToString();
         }
-
+        
         if (this.CompareTag("Score"))
         {
             score = Mathf.FloorToInt(score);
             scoreText.text = score.ToString();
+        }
+
+        if (this.CompareTag("Total"))
+        {
+            total = Mathf.FloorToInt(score) + bananaAmount;
+            totalText.text = total.ToString();
         }
     }
     
