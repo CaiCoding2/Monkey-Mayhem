6bc952c1f98e85bff445a449bba0def8567cea73
gmo3379 <gmo3379@gmail.com>
2018-11-04 19:31:31 -0600

    Newest

--- Monkey-Mayhem/Assets/Script/Credits.cs
+++ Monkey-Mayhem/Assets/Script/Credits.cs
@@ -14,5 +14,10 @@ public class Credits : MonoBehaviour {
 	private void Update()
 	{	
 		transform.Translate(Vector2.up * verticalSpeed * Time.deltaTime);
+
+		if (transform.position.y > 20)
+		{
+			transform.position = new Vector3(transform.position.x, -6.91f, transform.position.z);
+		}
 	}
 }
