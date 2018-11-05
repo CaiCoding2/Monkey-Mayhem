123af66f038fc11227380759bdbcaed617406f8d
gmo3379 <gmo3379@gmail.com>
2018-11-04 18:50:49 -0600

    credits

---
+++ Monkey-Mayhem/Assets/Script/Credits.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Credits : MonoBehaviour {
+
+	public float verticalSpeed;
+	
+	void Start ()
+	{
+		
+	}
+
+	private void Update()
+	{	
+		transform.Translate(Vector2.up * verticalSpeed * Time.deltaTime);
+	}
+}
