1692524f5a13538526c0d443212592beef97d0b1
Descritorio <36520867+Descritorio@users.noreply.github.com>
2018-11-06 13:32:59 -0600

    added DisplayHighscores.cs and Highscores.cs

---
+++ Monkey-Mayhem/Assets/Script/Highscores.cs
@@ -0,0 +1,104 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Highscores : MonoBehaviour
+{
+    /*
+     * How to implement: 
+     * The "Highscores.cs" and "DisplayHighscores.cs" should be added as a component
+     * into a gameobject. Then inside whatever text script that calculates the highscore call
+     * Highscores.AddNewHighscore(username, score);
+     * Have the user enter a username for the "username" field. 
+     * The highscore variable should be place into the "score" field.
+     * 
+     * Other notes:
+     * -Create a function to check for duplicate usernames
+     * -Create a function to add usernames if no username is entered
+     * -Implement a profanity checker
+     */
+
+    const string privateCode = "kBdKqPH-vUSFJot3a1oDUgyCEUYEYDlEun_HCls-ItWQ";
+    const string publicCode = "5be12ad261f94409f8bdb7fd";
+    const string webURL = "http://dreamlo.com/lb/";
+
+    DisplayHighscores highscoreDisplay;
+    public Highscore[] highscoresList;
+    static Highscores instance;
+
+    void Awake()
+    {
+        highscoreDisplay = GetComponent<DisplayHighscores>();
+        instance = this;
+    }
+
+    public static void AddNewHighscore(string username, int score)
+    {
+        instance.StartCoroutine(instance.UploadNewHighscore(username, score));
+    }
+
+    IEnumerator UploadNewHighscore(string username, int score)
+    {
+        WWW www = new WWW(webURL + privateCode + "/add/" + WWW.EscapeURL(username) + "/" + score);
+        yield return www;
+
+        if (string.IsNullOrEmpty(www.error))
+        {
+            print("Upload Successful");
+            DownloadHighscores();
+        }
+        else
+        {
+            print("Error uploading: " + www.error);
+        }
+    }
+
+    public void DownloadHighscores()
+    {
+        StartCoroutine("DownloadHighscoresFromDatabase");
+    }
+
+    IEnumerator DownloadHighscoresFromDatabase()
+    {
+        WWW www = new WWW(webURL + publicCode + "/pipe/");
+        yield return www;
+
+        if (string.IsNullOrEmpty(www.error))
+        {
+            FormatHighscores(www.text);
+            highscoreDisplay.OnHighscoresDownloaded(highscoresList);
+        }
+        else
+        {
+            print("Error Downloading: " + www.error);
+        }
+    }
+
+    void FormatHighscores(string textStream)
+    {
+        string[] entries = textStream.Split(new char[] { '\n' }, System.StringSplitOptions.RemoveEmptyEntries);
+        highscoresList = new Highscore[entries.Length];
+
+        for (int i = 0; i < entries.Length; i++)
+        {
+            string[] entryInfo = entries[i].Split(new char[] { '|' });
+            string username = entryInfo[0];
+            int score = int.Parse(entryInfo[1]);
+            highscoresList[i] = new Highscore(username, score);
+            print(highscoresList[i].username + ": " + highscoresList[i].score);
+        }
+    }
+
+}
+
+public struct Highscore
+{
+    public string username;
+    public int score;
+
+    public Highscore(string _username, int _score)
+    {
+        username = _username;
+        score = _score;
+    }
+
+}
