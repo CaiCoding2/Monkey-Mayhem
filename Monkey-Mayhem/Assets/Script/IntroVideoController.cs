90a0257d294f566b96e39ae6f1cb69155dcb1078
gmo3379 <gmo3379@gmail.com>
2018-11-06 11:02:20 -0600

    Intro Video

---
+++ Monkey-Mayhem/Assets/Script/IntroVideoController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class IntroVideoController : MonoBehaviour {
+
+    void Start()
+    {
+         StartCoroutine(LoadLevelAfterDelay(20));
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown (KeyCode.Space) 
+            || Input.GetKeyDown(KeyCode.Joystick1Button0) 
+            || Input.GetKeyDown(KeyCode.Joystick1Button1))
+        {
+            SceneManager.LoadScene("Menu");
+        }
+    }
+
+    IEnumerator LoadLevelAfterDelay(float delay)
+         {    
+             yield return new WaitForSeconds(delay);
+             SceneManager.LoadScene("Menu");            
+         }
+}
